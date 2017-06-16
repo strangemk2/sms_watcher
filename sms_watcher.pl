14bd196979051207e6da95821d79d38a1b55fdfb
Xi Juanjie <strangemk3@gmail.com>
2017-06-16 14:27:45 +0800

    Add logs.

--- sms_watcher.pl
+++ sms_watcher.pl
@@ -69,18 +69,19 @@ sub main
 	my $inotify = Linux::Inotify2->new() or die "Unable to create new inotify object: $!" ;
 	$inotify->watch ($cfg->param('COMMON.WATCH_FOLDER'), IN_MODIFY|IN_MOVED_TO|IN_CREATE) or die "Watch creation failed" ;
 
-	my $sms_mail_f = partial(\&send_sms_mail, $cfg);
+	my $sms_mail_f = partial(\&send_sms_mail, $cfg, $logger);
 	my $convert_mail_f = partial(\&sms_file_to_email,
 		$cfg->param('MAIL.FROM'),
 		$cfg->param('MAIL.TO'),
 		domain_name($cfg->param('MAIL.FROM')));
-	my $sms_watcher = partial(\&check_sms, $sms_mail_f, $convert_mail_f, $cfg->param('COMMON.WATCH_FOLDER'));
+	my $sms_watcher = partial(\&check_sms, $sms_mail_f, $convert_mail_f, $cfg->param('COMMON.WATCH_FOLDER'), $logger);
 
 	while (1)
 	{
 		my $timestamp = time();
 		my @events = $inotify->read();
 		die "read error: $!" if (@events == 0);
+		$logger->("Got folder inotified: " . $cfg->param('COMMON.WATCH_FOLDER'));
 		$sms_watcher->($timestamp);
 	}
 
@@ -110,33 +111,48 @@ sub sms_file_to_email($from, $to, $host, $sms_file)
 	$email->as_string();
 }
 
-sub check_sms($send_mail_f, $convert_mail_f, $sms_folder, $timestamp)
+sub check_sms($process_sms_f, $convert_mail_f, $sms_folder, $logger, $timestamp)
 {
 	my $wanted = sub
 	{
 		my $n = $File::Find::name;
-		$send_mail_f->($convert_mail_f->($n)) if (-f $n and (stat($n))[9] > $timestamp);
+		if (-f $n and (stat($n))[9] > $timestamp)
+		{
+			$logger->("Found new sms: $n");
+			$process_sms_f->($convert_mail_f->($n));
+		}
 	};
 
 	find($wanted, $sms_folder);
 }
 
-sub send_sms_mail($cfg, $sms_data)
+sub send_sms_mail($cfg, $logger, $sms_data)
 {
-	my $smtp = Net::SMTP->new($cfg->param('SMTP.SERVER'),
-		Port => $cfg->param('SMTP.PORT'),
-		SSL     => 1,
-		Timeout => $cfg->param('SMTP.TIMEOUT'),
-		Debug   => $cfg->param('SMTP.DEBUG'),
-		SSL_verify_mode => IO::Socket::SSL::SSL_VERIFY_NONE,
-	) or die $!;
-	$smtp->auth($cfg->param('SMTP.USER'), $cfg->param('SMTP.PASSWORD'));
-	$smtp->mail($cfg->param('MAIL.FROM'));
-	$smtp->to($cfg->param('MAIL.TO'));
-	$smtp->data();
-	$smtp->datasend($sms_data);
-	$smtp->dataend();
-	$smtp->quit();
+	eval
+	{
+		my $smtp = Net::SMTP->new($cfg->param('SMTP.SERVER'),
+			Port => $cfg->param('SMTP.PORT'),
+			SSL     => 1,
+			Timeout => $cfg->param('SMTP.TIMEOUT'),
+			Debug   => $cfg->param('SMTP.DEBUG'),
+			SSL_verify_mode => IO::Socket::SSL::SSL_VERIFY_NONE,
+		) or die $!;
+		$smtp->auth($cfg->param('SMTP.USER'), $cfg->param('SMTP.PASSWORD'));
+		$smtp->mail($cfg->param('MAIL.FROM'));
+		$smtp->to($cfg->param('MAIL.TO'));
+		$smtp->data();
+		$smtp->datasend($sms_data);
+		$smtp->dataend();
+		$smtp->quit();
+	};
+	if ($@)
+	{
+		$logger->("Sms mail have been sucessfully delivered.");
+	}
+	else
+	{
+		$logger->("Sms mail encounted error. $@");
+	}
 }
 
 main();
